9d5bdb635a290ab171acb1dd8071d3c747edb8d0
HeraldoArman <heraldoarman7@gmail.com>
2026-02-23 21:42:17 +0700

    fix: resolve case-sensitive template names and add PMD permissions

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -40,7 +40,7 @@ class ProductControllerTest {
     void testCreateProductPage() throws Exception {
         mockMvc.perform(get("/product/create"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("createProduct"))
+                .andExpect(view().name("CreateProduct"))
                 .andExpect(model().attributeExists("product"));
     }
 
@@ -60,7 +60,7 @@ class ProductControllerTest {
 
         mockMvc.perform(get("/product/list"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("productList"))
+                .andExpect(view().name("ProductList"))
                 .andExpect(model().attributeExists("products"))
                 .andExpect(model().attribute("products", allProducts));
     }
